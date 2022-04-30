8fabd3ba44257e484b44d4e6e836537e123ca90c
kema-ray <rachelkemuma99@gmail.com>
2022-04-30 12:05:29 +0300

    create a first template for the application

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-# from flask import render_template
-# from app import app
+from flask import render_template
+from app import app
 
-# @app.route('/') #localhost:5000
-# def index():
-#     pass
+@app.route('/') #localhost:5000
+def index():
+    return render_template('index.html')
