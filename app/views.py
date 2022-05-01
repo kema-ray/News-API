708654c60b0e3d94d2974b25e274c5a49c6f61dc
kema-ray <rachelkemuma99@gmail.com>
2022-05-01 16:21:28 +0300

    append styling to the cards

--- app/views.py
+++ app/views.py
@@ -4,11 +4,11 @@ from .request import get_articles,get_article
 
 @app.route('/') #localhost:5000
 def index():
-    entertainment_articles = get_articles('entertainment')
+    # political_articles = get_articles('political')
     health_articles = get_articles('health')
     technology_articles = get_articles('technology')
     business_articles = get_articles('business')
-    return render_template('index.html',health = health_articles,technology=technology_articles,business=business_articles,entertainment=entertainment_articles)
+    return render_template('index.html',health = health_articles,business = business_articles,technology = technology_articles)
 
 @app.route('/article/<name>')
 def article(name):
