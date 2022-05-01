dd49e045c2783073f2390c55a1afdb4b984c2f0a
kema-ray <rachelkemuma99@gmail.com>
2022-05-01 17:01:04 +0300

    create unittest for the article class

--- tests/test_article.py
+++ tests/test_article.py
@@ -10,11 +10,17 @@ class TestArticle(unittest.TestCase):
         """
         Set up method that will run before every test
         """
-        self.articles_object = Article('The Verge','Jay Peters','Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Teslas 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…','Its set to open later this year\r\nIf you buy something from a Verge link, Vox Media may earn a commission. See our ethics statement.\r\nIllustration by Alex Castro / The Verge\r\nBlock and Blockstream, a … [+1336 chars]','2022-04-29T13:15:44Z','https://www.engadget.com/beats-fit-pro-studio-buds-airpods-sale-131544330.html','"https://s.yimg.com/os/creatr-uploaded-images/2022-04/8b62a350-c7b8-11ec-a5af-d26c5ceedf70','Beats Fit Pro are on sale for $180 right now')
+        self.articles_object = Article('The Verge','Jay Peters','Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Teslas 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…','Its set to open later this year\r\nIf you buy something from a Verge link, Vox Media may earn a commission. See our ethics statement.\r\nIllustration by Alex Castro / The Verge\r\nBlock and Blockstream, a … [+1336 chars]','2022-04-29T13:15:44Z','https://www.engadget.com/beats-fit-pro-studio-buds-airpods-sale-131544330.html','https://s.yimg.com/os/creatr-uploaded-images/2022-04/8b62a350-c7b8-11ec-a5af-d26c5ceedf70','Beats Fit Pro are on sale for $180 right now')
     
     def test_instance(self):
         self.assertTrue(isinstance(self.articles_object,Article))
 
     def test_init(self):
         self.assertEqual(self.articles_object.name,'The Verge')
-        self.assertEqual(self.articles_object.name,'The Verge')
+        self.assertEqual(self.articles_object.author,'Jay Peters')
+        self.assertEqual(self.articles_object.description,'Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Teslas 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…')
+        self.assertEqual(self.articles_object.content,'Its set to open later this year\r\nIf you buy something from a Verge link, Vox Media may earn a commission. See our ethics statement.\r\nIllustration by Alex Castro / The Verge\r\nBlock and Blockstream, a … [+1336 chars]')
+        self.assertEqual(self.articles_object.publishedAt,'2022-04-29T13:15:44Z')
+        self.assertEqual(self.articles_object.url,'https://www.engadget.com/beats-fit-pro-studio-buds-airpods-sale-131544330.html')
+        self.assertEqual(self.articles_object.urlToImage,'https://s.yimg.com/os/creatr-uploaded-images/2022-04/8b62a350-c7b8-11ec-a5af-d26c5ceedf70')
+        self.assertEqual(self.articles_object.title,'Beats Fit Pro are on sale for $180 right now')
