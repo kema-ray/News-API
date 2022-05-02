08d65191650dfc6a929869af5a00dd8b53ee8e12
kema-ray <rachelkemuma99@gmail.com>
2022-05-02 19:45:42 +0300

    add a license

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .request import configure_request
+    configure_request(app)
+
     return app
 
 
